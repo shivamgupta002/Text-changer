7c9411569868132fbc24402cb6b37507839b85cb
shivamgupta002 <shivamgupta992909@gmail.com>
2023-10-20 12:45:54 +0530

    Header add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
-   <>
-   
-   </>
+    <>
+      <Header />
+    </>
   );
 }
 
